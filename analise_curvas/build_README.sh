e9fb965faa0b4a1e885b50295ee684773945489d
Gabriel Franco <gabrielfranco89@gmail.com>
2020-04-17 18:07:46 -0300

    Documento modelo derivadas em HTML

--- analise_curvas/build_README.sh
+++ analise_curvas/build_README.sh
@@ -2,7 +2,7 @@ sed -i 's/\\(/\$/g' modelo_derivadas.md
 sed -i 's/\\)/\$/g' modelo_derivadas.md
 sed -i 's/\\\[/\$\$/g' modelo_derivadas.md
 sed -i 's/\\\]/\$\$/g' modelo_derivadas.md
-python -m readme2tex --output README.md modelo_derivadas.md
+python -m readme2tex --nocdn --output README.md modelo_derivadas.md
 
 # substitute 
 # https://rawgit.com/in	git@github.com:labbcb/covid19unicamp/None
